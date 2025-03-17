6286cbc3061ef1db5d33cfff7738e731b381c35a
DussaUday <dussauday469@gmail.com>
2025-03-17 20:04:41 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -26,7 +26,7 @@ export const SocketContextProvider = ({ children }) => {
 	const {setFollowersDetails}=useState([]);
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://socialty-api.vercel.app", {
+			const socket = io("https://socialty.vercel.app", {
 				query: {
 					userId: authUser._id,
 				},
