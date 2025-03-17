0f79f9ba99e3dbcce1ed6c15b7d8aea5873bd867
DussaUday <dussauday469@gmail.com>
2025-03-17 15:19:22 +0530

    first-commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : process.env.VITE_BASE_PATH || "/socialty",
-  server:{
+  base: process.env.VITE_BASE_PATH || "/socialty",
+  server: {
     port: 3000,
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000",
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
-})
+});
