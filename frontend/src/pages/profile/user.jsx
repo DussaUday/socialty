88b4fcd3dcaf0aef79eae7a7cccdaf5ce26a2b65
DussaUday <dussauday469@gmail.com>
2025-03-18 20:56:32 +0530

    Update user.jsx

--- frontend/src/pages/profile/user.jsx
+++ frontend/src/pages/profile/user.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const updateProfile = async (userId, data) => {
     try {
-        const response = await axios.put(`https://socialty.vercel.app/api/users/${userId}`, data);
+        const response = await axios.put(`/api/users/${userId}`, data);
         return response.data;
     } catch (error) {
         console.error("Error updating profile:", error);
