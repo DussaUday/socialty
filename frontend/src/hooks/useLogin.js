b38ea514abe90dee6c402416754dfc44d1582bc5
DussaUday <dussauday469@gmail.com>
2025-03-19 11:59:28 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -18,6 +18,7 @@ const useLogin = () => {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password }),
+                credentials: "include"
             });
 
             if (!res.ok) {
